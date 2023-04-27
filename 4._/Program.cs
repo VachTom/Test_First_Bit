1eacf3bed19b15870c90422f4c16e90c4b0791b8
Vachik_Tovmasyan <tovm2504@mail.ru>
2023-04-27 17:14:13 +0300

    4.

--- 4._/Program.cs
+++ 4._/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*4. Составить алгоритм, вычисляющий значение функции F(n) = 1!*2!*3!*…*n!, где n! = 
+1*2*3*…*n. Можно использовать только один оператор цикла*/
+
+double Factorial(int n)
+{
+    // 1! = 1;
+    // 0! = 1;
+    if (n == 1 || n == 0) return 1;
+    else return n * Factorial(n - 1);
+}
+
+Console.WriteLine(Factorial(6));
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
